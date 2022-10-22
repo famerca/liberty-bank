4efb75480bcacd451c57387dd0f3f4f14569bb85
danyr59 <danyelrange59@gmail.com>
2022-10-22 18:21:38 -0400

    input on change

--- src/components/Movimientos.jsx
+++ src/components/Movimientos.jsx
@@ -21,40 +21,41 @@ let datosCategorias = [
 ]
 
 let datosCuentas = [
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
-  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 1 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
+  { nombreCuenta: "nombre de cuenta", numero: "0108-0113-0710823", titular: "bobby", banco: "jp morgan", id: 2 },
 ]
 
 const Table = (props) => {
   const [datos, setDatos] = useState([])
-  const tipo = useState(props.type)
+  const [tipo, setTipo] = useState("")
 
   useEffect(() => {
     //   //fetch a api
     //   //api que va a traer datos del backend
     //   // se inicializan los datos en caso de existir
     setDatos([...props.datos])
+    setTipo(props.type)
   }, [])
 
   const head = tipo == "movimientos" ?
@@ -87,8 +88,8 @@ const Table = (props) => {
         </thead>
         <tbody>
           {/* <Row data={datos} /> */}
-          {!datos ? <label> loading ....</label> :
-            <Row data={datos} />
+          {datos.length == 0 ? <label> loading ....</label> :
+            <Rows data={datos} tipo={tipo} />
           }
         </tbody>
       </table>
@@ -98,33 +99,62 @@ const Table = (props) => {
 
 }
 
+const RowData = (props) => {
+  const { index } = props
+  const { type } = props
+  const [nombre, setNombre] = useState(props.type == "movimientos" ? props.row.nombre : props.row.nombreCuenta)
+  const [tipo, setTipo] = useState(props.row.tipo)
+  const [id] = useState(props.row.id)
+  const [numero, setNumero] = useState(props.row.numero)
+  const [titular, setTitular] = useState(props.row.titular)
+  const [banco, setBanco] = useState(props.row.banco)
 
-const Row = (props) => {
+
+  console.log(index)
+  //aqui realizo una solicitud post cuando se modifiquen los datos 
+
+  return (
+    <tr>
+      {type == "movimientos" ?
+        <>
+          <td><input onChange={(e) => setNombre(e.target.value)} defaultValue={nombre} /></td>
+          <td><input onChange={(e) => setTipo(e.target.value)} defaultValue={tipo} /></td>
+          <td>{id}</td>
+          <td><button><VscError size={20} /> </button></td>
+        </>
+        :
+        <>
+          <td><input onChange={(e) => setNombre(e.target.value)} defaultValue={nombre} /></td>
+          <td><input onChange={(e) => setNumero(e.target.value)} defaultValue={numero} /></td>
+          <td><input onChange={(e) => setTitular(e.target.value)} defaultValue={titular} /></td>
+          <td><input onChange={(e) => setBanco(e.target.value)} defaultValue={banco} /></td>
+          <td>{id}</td>
+          <td><button> <VscError size={20} /></button></td>
+        </>
+      }
+    </tr>
+  )
+}
+
+const Rows = (props) => {
 
   const [datos, setDatos] = useState([])
+  const { tipo } = props
   useEffect(() => {
     setDatos([...props.data])
   }, [props.data])
 
-  console.log(datos)
-  const setField = (e) => {
-    console.log(e)
+  return (
+    <>
+      {datos.map((row, index) => {
+        return (
+          <RowData index={index} key={index} type={tipo} row={row} />
+        )
+      })}
+    </>
 
-  }
-  // console.log(setDatos)
-  // const datos_ = datos ? datos.map((dato, index) => {
-  //   const values = Object.values(datos).map((value, index) => {
-  //     return (<td key={index}><input onChange={setField} key={index} defaultValue={value} /></td>)
-  //   })
-  //   return (
-  //     <tr key={index}>
-  //       {values}
-  //       <td><button><VscError size={20} /></button></td>
-  //     </tr>
-  //   )
-  // }) : null
+  )
 
-  return <div> hola</div>
 }
 
 
@@ -138,30 +168,4 @@ const Movimientos = () => {
 }
 
 export default Movimientos
-// const TableCategory = (props) => {
-//   return (
-//     <div>
-//       <div style={{ display: "flex" }}>
-//         <h1 style={{ fontWeight: 1000, fontSize: "20px" }} >Categorias</h1>
-//         <button style={{ border: "none" }}><AiFillPlusCircle size={20} /></button>
-//       </div>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Nombre</th>
-//             <th>Tipo</th>
-//             <th>id</th>
-//             <th>Acciones</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           <RowCategory data={datos} />
-//         </tbody>
-//       </table>
-//     </div>
-
-//   )
-
-
-// }
 
