0a7fe1cd6b5db325db5db4feb959427845050e4b
JheniCT <115767197+JheniCT@users.noreply.github.com>
2022-11-03 15:47:46 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ const Header = () =>{
     return (
         <header className="App-header">
             <div className="button-container">
+                <Link to='/aggmovimientos'>
                 <button className="outline-button"> <BiPlusCircle/> Agregar Movimiento</button>
+                </Link>
             </div>
         </header>
     )
